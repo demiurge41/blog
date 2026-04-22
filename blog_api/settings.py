18bc106f12a357027b4755433866479125a1e245
demiurge41 <zari736988@gmail.com>
2026-04-22 22:00:38 +0600

    test2

--- blog_api/settings.py
+++ blog_api/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6^!!$q=(*r8*^cz9t0#(3+5&!zz+u7fara4fe$u8w8xb%w!#ta'
+SECRET_KEY = os.environ.get('SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- blog_api/urls.py
+++ blog_api/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include, re_path
-from .swagger import schema_view
+from django.urls import path, include
+from . import swagger   
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('blog.urls')),
     path('api/v1/users/', include('users.urls')),
-
 ]
+
+urlpatterns += swagger.urlpatterns  
